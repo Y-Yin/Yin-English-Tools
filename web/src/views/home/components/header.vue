76d2953899641dd182a34f344746d83f0b456c26
hy <1244957019@qq.com>
2023-06-26 14:28:40 +0800

    web first commit

---
+++ web/src/views/home/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+    我是头部   
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ web/src/views/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-box">
+        <!-- 头部组件 -->
+        <Header></Header>
+        <!-- 搜索组件 -->
+
+        <!-- 列表 -->
+    </div>
+</template>
+ 
+
+<script setup>
+import { ref } from 'vue';
+import Header from './components/header.vue';
+</script>
+
+
+<style lang="scss" scoped>
+.home-box{
+    width: 100%;
+    min-height: 100vh;
+    background: wheat;
+    display: flex;
+    flex-direction: column;
+}
+</style>
